61a8894896f6a7e5cc6c9377a5d85166f7ddfe73
blytheb <builtbyblythe@gmail.com>
2025-08-29 13:03:21 -1000

    automatically goes to profile page if logged in

--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import React from 'react';
+import { useEffect } from 'react';
+import { useNavigate } from 'react-router';
+import { useAuth } from '../hooks/useAuth.js';
+
 import LoginForm from '../components/auth/LoginForm.jsx';
 import { Link } from 'react-router';
 import TokenTestButton from '../components/auth/TokenTestButton';
 
 const LoginPage = () => {
+  const { user } = useAuth();
+  const navigate = useNavigate();
+
+  //if alreadu logged in, redirect to profiles
+  useEffect(() => {
+    if (user) {
+      navigate("/profiles", {replace: true});
+    }  
+  }, [user, navigate]);
+
   return (
     <div className='bg-login h-screen flex items-center justify-center'>
         <div className='bg-black bg-opacity-70 p-10 rounded-lg shadow-lg q-96 text-white'>
